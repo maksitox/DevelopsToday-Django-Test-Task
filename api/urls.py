95389909d2c4186d9ea5a04f476c14af074e5048
Maks <max.poliuha@gmail.com>
2026-02-12 17:50:33 +0200

    feat: Add pagination and search filtering

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TravelProjectViewSet, PlaceViewSet
+
+router = DefaultRouter()
+router.register(r'projects', TravelProjectViewSet, basename='project')
+router.register(r'places', PlaceViewSet, basename='place')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
